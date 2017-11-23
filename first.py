a6d2e26da88799aea3a7c3f44d0c9d5f33085db9
Omer Dagan <omerd@riscogroup.com>
2017-11-23 15:54:58 +0200

    add detailed comments to the codew
    Signed-off-by: Omer Dagan <omerd@riscogroup.com>

--- first.py
+++ first.py
@@ -3,24 +3,37 @@
 
 import tkinter
 
-class simpleapp_tk(tkinter.Tk):
-    def __init__(self,parent):
-        tkinter.Tk.__init__(self,parent)
-        self.initialize()
+class simpleapp_tk(tkinter.Tk):     #app defined as a class
+    def __init__(self,parent):          #define the init function of the class
+        tkinter.Tk.__init__(self,parent)    #call the tkinter initializer
+        self.initialize()                   #call the app initializer
 
-    def initialize(self):
+    def initialize(self):   #the app initializer function           
+
+        #call the grid layout manager
+        #this enables us to arrange the objects on a grid 
         self.grid()
 
+        #create a text box 'Entry' set parent as self
+        #note that we keep a referance of it
         self.entry = tkinter.Entry(self)
+        #place the item on the grid at location 0,0 stick to left and right
         self.entry.grid(column=0,row=0,sticky='EW')
 
+        #create a button
+        #set parent to self and set the text
         button = tkinter.Button(self,text=u"Click me !")
+        #place the item on the grid at location 0,1 
         button.grid(column=1,row=0)
 
+        #create a label 
+        #anchor - sets text alignment to left
+        #fg - foreground color bg - background color
         label = tkinter.Label(self,anchor="w",fg="white",bg="blue")
+        #place the item on the grid at location 1,0 spanning on two columns 
         label.grid(column=0,row=1,columnspan=2,sticky='EW')
 
 if __name__ == "__main__":
-    app = simpleapp_tk(None)
-    app.title('my application')
-    app.mainloop()
+    app = simpleapp_tk(None)    #start the app
+    app.title('my application') #set the window lable
+    app.mainloop()              #call mainloop
