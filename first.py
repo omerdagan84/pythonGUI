972303489c3a97d3fa9be38f161442e4646959b6
Omer Dagan <omerd@riscogroup.com>
2017-11-23 15:43:45 +0200

    basic app - text, button and lable
    Signed-off-by: Omer Dagan <omerd@riscogroup.com>

--- first.py
+++ first.py
@@ -1,4 +1,26 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-1 -*-
 
-import Tkinter
+import tkinter
+
+class simpleapp_tk(tkinter.Tk):
+    def __init__(self,parent):
+        tkinter.Tk.__init__(self,parent)
+        self.initialize()
+
+    def initialize(self):
+        self.grid()
+
+        self.entry = tkinter.Entry(self)
+        self.entry.grid(column=0,row=0,sticky='EW')
+
+        button = tkinter.Button(self,text=u"Click me !")
+        button.grid(column=1,row=0)
+
+        label = tkinter.Label(self,anchor="w",fg="white",bg="blue")
+        label.grid(column=0,row=1,columnspan=2,sticky='EW')
+
+if __name__ == "__main__":
+    app = simpleapp_tk(None)
+    app.title('my application')
+    app.mainloop()
